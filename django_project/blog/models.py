1a7ccb1e61bd0258227298d71bc0332afb82ffa6
Nakagawa Yuto <n.yuuto.11.11@docomo.ne.jp>
2023-10-16 17:14:41 +0900

    add comment backend

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -6,3 +6,8 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     url = models.URLField(blank=True, null=True)
     body = models.TextField()
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment = models.TextField(blank=False, null=False)
