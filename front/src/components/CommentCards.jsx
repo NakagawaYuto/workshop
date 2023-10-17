fe2c1b1040d39a5eaddd9bd45a26352579e51154
Nakagawa Yuto <n.yuuto.11.11@docomo.ne.jp>
2023-10-17 17:40:24 +0900

    refactor

--- front/src/components/CommentCards.jsx
+++ front/src/components/CommentCards.jsx
@@ -1,32 +1,29 @@
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+// import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardActionArea';
 
 const CommentCards = ({ Comments }) => {
-  var Cards = [];
+  var Typographys = [];
   for (let i = 0; i < Comments.length; i++) {
     const comment = Comments[i]
-    Cards.push(
-      <Grid item key={comment.id}>
-        <Card sx={{ maxWidth: 800 }} elevation={4} style={{margin: 10}}>
-          <CardActionArea >
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                { comment.comment }
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid>
+    Typographys.push(
+      <Typography gutterBottom variant="h5" component="div" style={{fontSize: 18}}>
+        { comment.comment }
+      </Typography>
     );
   }
   return (
     <>
-      <Grid container alignItems='center' justify='center' direction="column">
-        { Cards }
-      </Grid>
+      {/* <Card container alignItems='center' justify='center' direction="column">
+        { Typographys }
+      </Card> */}
+      <Card sx={{ maxWidth: 800 }} elevation={0} style={{margin: 30, width: '40%', backgroundColor: '#F5DEB3'}}>
+        <CardContent>
+          { Typographys }
+        </CardContent>
+      </Card>
     </>
   )
 }
